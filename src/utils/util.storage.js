91c9f55df2dd5a33c030b6b644b3d0232f73570f
TomDiary <dsyafda@qq.com>
2021-09-13 02:01:42 +0800

    封装storage

---
+++ src/utils/util.storage.js
@@ -0,0 +1,34 @@
+/**
+ * @description 公共Storage
+ * @author TomDiary
+ * @link https://www.7b3.rog or https://github.com/tomdiary
+ */
+export const cvLocalStorage = {
+  get(type) {
+    return JSON.parse(localStorage.getItem(type))
+  },
+  set(type, value) {
+    return localStorage.setItem(type, JSON.stringify(value))
+  },
+  remove(type) {
+    return localStorage.remove(type)
+  },
+  clear() {
+    return localStorage.clear()
+  }
+}
+
+export const cvSessionStorage = {
+  get(type) {
+    return JSON.parse(sessionStorage.getItem(type))
+  },
+  set(type, value) {
+    return sessionStorage.setItem(type, JSON.stringify(value))
+  },
+  remove(type) {
+    return sessionStorage.remove(type)
+  },
+  clear() {
+    return sessionStorage.clear()
+  }
+}
