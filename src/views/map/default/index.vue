0413dbc25692a44845ebf955432d7f25b5f6d57f
TomDiary <dsyafda@qq.com>
2022-06-02 18:37:45 +0800

    feat: 增加高德配置，添加默认地图页面

--- src/views/map/default/index.vue
+++ src/views/map/default/index.vue
@@ -1,8 +1,26 @@
 <template>
+  <cv-page-layout :isFooter="false" :isHeader="false">
+    <template #default="scope">
+      <div id="container" :style="{ height: `${scope.mainHeight}px` }"></div>
+    </template>
+  </cv-page-layout>
 </template>
 
 <script setup>
-</script>
+import { onMounted, onUnmounted, ref } from 'vue'
+import { lazyAMapApiLoaderInstance } from '@vuemap/vue-amap'
 
-<style scoped lang="scss">
-</style>
+const map = ref(null)
+
+onMounted(() => {
+  initBaseMap()
+})
+
+onUnmounted(() => console.log(map)) // 待优化，需要增加销毁地图功能
+
+const initBaseMap = () => {
+  lazyAMapApiLoaderInstance.then(() => {
+    map.value = new AMap.Map('container')
+  })
+}
+</script>
