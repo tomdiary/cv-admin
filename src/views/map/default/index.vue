fd1e67fa8b7be17bddda81b7b082593e10776a46
TomDiary <dsyafda@qq.com>
2022-06-05 01:11:52 +0800

    feat: 高德地图增加自动换肤功能

--- src/views/map/default/index.vue
+++ src/views/map/default/index.vue
@@ -9,8 +9,10 @@
 <script setup>
 import { onMounted, onUnmounted, ref } from 'vue'
 import { lazyAMapApiLoaderInstance } from '@vuemap/vue-amap'
+import useLayoutStore from '@/store/layout'
 
 const map = ref(null)
+const layoutState = useLayoutStore()
 
 onMounted(() => {
   initBaseMap()
@@ -18,10 +20,14 @@ onMounted(() => {
 
 onUnmounted(() => console.log(map)) // 待优化，需要增加销毁地图功能
 
+layoutState.$subscribe((newVal, oldVal) => {
+  if (newVal.events.key === 'themeMode') initBaseMap()
+}, { detached: true })
+
 const initBaseMap = () => {
   lazyAMapApiLoaderInstance.then(() => {
     map.value = new AMap.Map('container', {
-      mapStyle: import.meta.env.VITE_AMAP_DARK_STYLES
+      mapStyle: layoutState.themeMode === 'light' ? import.meta.env.VITE_AMAP_LIGHT_STYLES : import.meta.env.VITE_AMAP_DARK_STYLES
     })
   })
 }
