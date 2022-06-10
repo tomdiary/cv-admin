63fc35bef0b7f3335fbe322f3143c65616589afe
TomDiary <dsyafda@qq.com>
2022-06-10 22:19:55 +0800

    fix: 更新element-plus、Vue、pinia、axios版本

--- components.d.ts
+++ components.d.ts
@@ -23,6 +23,7 @@ declare module '@vue/runtime-core' {
     ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElPagination: typeof import('element-plus/es')['ElPagination']
+    ElRow: typeof import('element-plus/es')['ElRow']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
