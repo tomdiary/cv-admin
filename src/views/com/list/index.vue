979fb05369eb38f808693356b1b35f3fea14ed56
TomDiary <dsyafda@qq.com>
2022-06-14 22:44:03 +0800

    test: 测试 cv-page-layout 组件 isScrollY 功能

--- src/views/com/list/index.vue
+++ src/views/com/list/index.vue
@@ -1,1 +1,25 @@
-<template>组件列表</template>
+<template>
+  <cv-page-layout isScrollY :is-header="false" :is-footer="false">
+    <template #default="scope">
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+      <h1>测试</h1>
+    </template>
+    <template #footer>
+      <el-button type="primary">底部按钮</el-button>
+    </template>
+  </cv-page-layout>
+</template>
