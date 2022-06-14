a8a6fe2eef56eaf29257cd62011ad79156fa0571
TomDiary <dsyafda@qq.com>
2022-06-14 22:42:22 +0800

    feat: 动态调整 AppMain 内边距

--- src/layout/compoents/AppMain/index.vue
+++ src/layout/compoents/AppMain/index.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="app-main">
+  <div class="app-main" :class="layoutType">
     <router-view />
   </div>
 </template>
 
+<script setup>
+import { ref, onBeforeMount } from 'vue'
+import { useRoute, onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router'
+
+const route = useRoute()
+const layoutType = ref('main-is-padding')
+
+onBeforeMount(() => {
+  initLayoutPadding(route.meta.layoutPadding)
+})
+
+onBeforeRouteLeave((to, from) => {
+  initLayoutPadding(to.meta.layoutPadding)
+})
+
+onBeforeRouteUpdate((to, from) => {
+  initLayoutPadding(to.meta.layoutPadding)
+})
+
+// 根据路由元信息(meta)中的 layoutPadding 判断是否需要 padding，true-需要，false-不需要，如果为空默认需要
+const initLayoutPadding = status => {
+  layoutType.value = typeof status === 'boolean' && !status ? 'main-no-padding' : 'main-is-padding'
+}
+</script>
+
 <style scoped lang="scss">
 .app-main {
+}
+
+.main-is-padding {
   height: calc(100% - #{$tags-view-hei + $main-padding * 2});
   padding: $main-padding;
+}
+
+.main-no-padding {
+  height: calc(100% - #{$tags-view-hei});
 }
 </style>
