aaa05df3f56b78cbfc81097fbd71f47a401b9d80
TomDiary <dsyafda@qq.com>
2022-08-07 23:44:24 +0800

    Update _utils.js

--- mock/_utils.js
+++ mock/_utils.js
@@ -2,6 +2,7 @@ import { encode, decode } from 'js-base64'
 import { SECRET_KEY, ACCESS_TOKEN_TIME, REFRESH_TOKEN_TIME } from './_config'
 import moment from 'moment'
 
+// 生成 AccessToken
 export const encryptAccessToken = data => {
   const tokenData = {
     userInfo: data,
@@ -11,8 +12,10 @@ export const encryptAccessToken = data => {
   return encode(JSON.stringify(tokenData))
 }
 
+// 解密 AccessToken
 export const decryptAccessToken = data => JSON.parse(decode(data))
 
+// 生成 RefreshToken
 export const encryptRefreshToken = data => {
   const tokenData = {
     userInfo: data,
@@ -22,4 +25,16 @@ export const encryptRefreshToken = data => {
   return encode(JSON.stringify(tokenData))
 }
 
+// 解密 RefreshToken
+export const decryptRefreshToken = data => JSON.parse(decode(data))
+
+// 随机数
 export const genBetweenAll = (m, n) => Math.floor(Math.random() * (n - m + 1)) + m
+
+// 检测 token 是否到期
+export const detectTokenExpiration = data => {
+  if (!data.authorization) return 1
+  const token = data.authorization.split(' ')
+  if (token.length < 1) return 1
+  // decryptAccessToken(data.authorization)
+}
