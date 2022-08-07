4426d5f018a87bc1800ba242c04381caeb74c658
TomDiary <dsyafda@qq.com>
2022-08-07 23:44:31 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-config-provider :size="layoutState.gtThemeSize">
+  <el-config-provider :locale="locale" :size="layoutState.gtThemeSize">
     <div id="app">
       <router-view />
     </div>
@@ -7,7 +7,24 @@
 </template>
 
 <script setup>
+import { watch, onMounted, ref } from 'vue'
 import useLayoutStore from '@/store/layout'
+import zhCnLocale from 'element-plus/lib/locale/lang/zh-cn'
+import zhTwLocale from 'element-plus/lib/locale/lang/zh-tw'
+import enLocale from 'element-plus/lib/locale/lang/en'
+import jaLocale from 'element-plus/lib/locale/lang/ja'
+import { languageList } from '@/config/dataSource'
 
+const locale = ref('')
 const layoutState = useLayoutStore()
+
+watch(() => layoutState.themeLanguage, (newVal) => {
+  console.log(newVal, languageList)
+})
+
+onMounted(() => initBaseData())
+
+const initBaseData = () => {
+  const item = ""
+}
 </script>
