57c6140256ee682c53a1f51dc21f93e2ade58b6a
TomDiary <dsyafda@qq.com>
2022-08-30 00:29:45 +0800

    fix: 🔧  调试 page-layout 组件

--- src/views/com/list/index.vue
+++ src/views/com/list/index.vue
@@ -1,6 +1,7 @@
 <template>
   <cv-page-layout isScrollY :is-header="false" :is-footer="false">
     <template #default="scope">
+      {{ scope }}
       <h1>测试</h1>
       <h1>测试</h1>
       <h1>测试</h1>
@@ -17,9 +18,6 @@
       <h1>测试</h1>
       <h1>测试</h1>
       <h1>测试</h1>
-    </template>
-    <template #footer>
-      <el-button type="primary">底部按钮</el-button>
-    </template>
+     </template>
   </cv-page-layout>
 </template>
