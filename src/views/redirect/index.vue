c4ebfac53f8681dd30c56eb7647120539bdb880f
TomDiary <dsyafda@qq.com>
2022-08-31 00:16:48 +0800

    🌿 feat: 增加 redirect 页面

---
+++ src/views/redirect/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+onMounted(() => {
+  const route = useRoute()
+  const router = useRouter()
+  const { params, query } = route
+  const { path } = params
+  router.replace({ path: `/${path}`, query })
+})
+</script>
