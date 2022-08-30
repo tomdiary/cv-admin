88cf37fc7560c099c3b9267f01e838c0c73dcd1b
TomDiary <dsyafda@qq.com>
2022-08-31 00:15:34 +0800

    ✨ fix: 优化国际化刷新页面的逻辑

--- src/language/ja.js
+++ src/language/ja.js
@@ -5,5 +5,16 @@ export default {
   query: 'お問い合わせ',
   reset: 'リセット',
   export: '輸出',
-  import: '輸入'
+  import: '輸入',
+  inspirational: '世界は変えられないが、自分は変えられる',
+  tableView: {
+    default: {
+      vin: 'VIN',
+      carNum: 'ナンバープレート',
+      terminal: '端子番号',
+      createTime: '作成時間',
+      address: '住所',
+      customerName: '取引先'
+    }
+  }
 }
