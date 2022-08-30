11ae9255e3a844827326d1a2eed37c5067287094
TomDiary <dsyafda@qq.com>
2022-08-31 00:15:26 +0800

    ✨ fix: 优化国际化刷新页面的逻辑

--- src/language/es.js
+++ src/language/es.js
@@ -5,5 +5,16 @@ export default {
   query: 'Consulta',
   reset: 'Restablecer',
   export: 'Exportar',
-  import: 'Importar'
+  import: 'Importar',
+  inspirational: 'No puedes cambiar el mundo, pero puedes cambiarte a ti mismo',
+  tableView: {
+    default: {
+      vin: 'VIN',
+      carNum: 'Placa de matrícula',
+      terminal: 'Número de terminal',
+      createTime: 'Tiempo de creación',
+      address: 'Dirección',
+      customerName: 'Clientes'
+    }
+  }
 }
