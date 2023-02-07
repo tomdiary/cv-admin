0030bcc83e5c7e3de86446c01905a0b2f63f6524
TomDiary <37231772+tomdiary@users.noreply.github.com>
2023-02-07 17:01:42 +0800

    feat: 增加 ESLint 规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,22 +6,45 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-essential'
+    'plugin:vue/vue3-essential',
+    'plugin:import/recommended'
   ],
   overrides: [],
   parserOptions: {
     'ecmaVersion': 'latest',
     'sourceType': 'module'
   },
+  globals: {
+    $api: true,
+    defineProps: true
+  },
   plugins: [
     'vue'
   ],
+  settings: {
+    // https://www.npmjs.com/package/eslint-import-resolver-alias
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+          ['@lay', './src/layout'],
+          ['@com', './src/components'],
+          ['@ld', './src/layout/Default/index.vue']
+        ],
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+      }
+    }
+  },
   rules: {
     semi: [2, 'never'],
     'import/prefer-default-export': 0,
     'import/no-unresolved': [
       2,
-      {caseSensitive: false}
+      {
+        commonjs: true,
+        amd: true,
+        caseSensitive: false
+      }
     ],
     'import/extensions': [
       'error',
@@ -32,6 +55,10 @@ module.exports = {
         ts: 'never',
         tsx: 'never'
       }
-    ]
+    ],
+    'import/named': 0,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2
   }
 }
