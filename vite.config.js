241d8f795eb9659063bcb82fb5d4a116d0407f27
TomDiary <andihankai@gmail.com>
2023-04-14 18:10:22 +0800

    chore: mock配置文件变动

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ import { svgBuilder } from './src/utils/svgBuilder.js'
 const pathResolve = dir => resolve(process.cwd(), '.', dir)
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
+export default ({ mode, command }) => {
   const env = loadEnv(mode, process.cwd())
   const localEnabled = env.VITE_USE_MOCK || false
   const prodEnabled = env.VITE_USE_CHUNK_MOCK || false
@@ -28,16 +28,16 @@ export default ({ mode }) => {
       vue(),
       // Mock
       viteMockServe({
-        ignore: /^\_/, // 忽略以_开头的文件
-        mockPath: 'mock', // mock 文件地址
+        mockPath: './mock', // mock 文件地址
         logger: true, // 控制台请求日志
         watchFiles: true, // 监视文件更改
+        supportTs: false, // 是否读取 ts 文件，开启后无法监听 js 文件
+        localEnabled: command === 'serve', // 本地是否启用 ts 文件，设置为 false 将禁用 mock 功能
+        prodEnabled: command !== 'serve' && prodEnabled, // 打包后是否启用 mock 功能
         injectCode: `
           import { setupProdMockServer } from '../mock/_createServer.js'
           setupProdMockServer()
-        `,
-        localEnabled,
-        prodEnabled
+        ` // 如果打包后开启 mock, injectCode 会将对应的文件注入生产环境中
       }),
       // 自动引入组件对应的 CSS 样式或者 SASS 文件
       ElementPlus({
