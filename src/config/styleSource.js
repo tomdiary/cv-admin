310a925bd5d070e953911f15f826d33bda02ccde
TomDiary <andihankai@gmail.com>
2023-04-20 02:33:34 +0800

    feat: 增加主题配色

---
+++ src/config/styleSource.js
@@ -0,0 +1,62 @@
+/**
+ * 配色
+ * @author TomDiary
+ * @createTime 2023-04-19 21:38
+ */
+
+// 绿黑
+export const BASIC_STYLE_GREEN_WHITE = {
+  Cv_Color_Primary: '#41B584', // 主色
+  Cv_Color_Success: '#7DCE2D', // 辅色
+  Cv_Color_Warning: '#FFA202', // 辅色
+  Cv_Color_Danger: '#FF3863', // 辅色
+  Cv_Color_Info: '#1956FF', // 辅色
+  Cv_Color_Error: '#F56C6C', // 辅色
+  Cv_Bg_Color: '#292C34', // 全局背景色
+  Cv_Sidebar_Bg_Color: '#292C34', // 侧边栏菜单背景色
+  Cv_Sidebar_Font_Color: '#292C34', // 侧边栏菜单字体颜色
+  Cv_Sidebar_Font_Active_Color: '#292C34', // 侧边栏菜单激活时字体颜色
+}
+
+// 绿白
+export const BASIC_STYLE_GREEN_BLACK = {
+  Cv_Color_Primary: '#41B584', // 主色
+  Cv_Color_Success: '#7DCE2D', // 辅色
+  Cv_Color_Warning: '#FFA202', // 辅色
+  Cv_Color_Danger: '#FF3863', // 辅色
+  Cv_Color_Info: '#1956FF', // 辅色
+  Cv_Color_Error: '#F56C6C', // 辅色
+  Cv_Bg_Color: '#FFFFFF', // 全局背景色
+  Cv_Sidebar_Bg_Color: '#292C34', // 侧边栏菜单背景色
+  Cv_Sidebar_Font_Color: '#292C34', // 侧边栏菜单字体颜色
+  Cv_Sidebar_Font_Active_Color: '#292C34', // 侧边栏菜单激活时字体颜色
+}
+
+// 红白
+export const BASIC_STYLE_RED_WHITE = {
+  Cv_Color_Primary: '#E84545', // 主色
+  Cv_Color_Success: '#6DC130', // 辅色
+  Cv_Color_Warning: '#C3CC1A', // 辅色
+  Cv_Color_Danger: '#E04435', // 辅色
+  Cv_Color_Info: '#02AFDB', // 辅色
+  Cv_Color_Error: '#F56C6C', // 辅色
+  Cv_Bg_Color: '#FFFFFF', // 全局背景色
+  Cv_Sidebar_Bg_Color: '#292C34', // 侧边栏菜单背景色
+  Cv_Sidebar_Font_Color: '#292C34', // 侧边栏菜单字体颜色
+  Cv_Sidebar_Font_Active_Color: '#292C34', // 侧边栏菜单激活时字体颜色
+}
+
+// 红黑
+export const BASIC_STYLE_RED_BLACK = {
+  Cv_Color_Primary: '#E84545', // 主色
+  Cv_Color_Success: '#6DC130', // 辅色
+  Cv_Color_Warning: '#C3CC1A', // 辅色
+  Cv_Color_Danger: '#E04435', // 辅色
+  Cv_Color_Info: '#02AFDB', // 辅色
+  Cv_Color_Error: '#F56C6C', // 辅色
+  Cv_Bg_Color: '#292C34', // 全局背景色
+  Cv_Sidebar_Bg_Color: '#292C34', // 侧边栏菜单背景色
+  Cv_Sidebar_Font_Color: '#292C34', // 侧边栏菜单字体颜色
+  Cv_Sidebar_Font_Active_Color: '#292C34', // 侧边栏菜单激活时字体颜色
+}
+
