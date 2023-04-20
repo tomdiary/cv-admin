ebfa40bf4890a22a43d33d8081b8ba4d2d6b7e1c
TomDiary <andihankai@gmail.com>
2023-04-20 19:10:56 +0800

    feat: 全局挂载 utils

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { initDirective } from '@/directive'
 import { initConfig } from '@/config'
 import { initLanguage } from '@/language'
 import { initRouter } from '@/router'
+import * as basicUtils from '@/utils'
 import App from '@/App.vue'
 import api from '@/api'
 import '@/styles/index.scss'
@@ -18,6 +19,8 @@ async function bootstrap() {
   const app = createApp(App)
   // 全局挂载api
   window.$api = api
+  window.$bu = basicUtils
+
   initRouter(app) // 初始化路由
   initComponents(app) // 初始化组件
   initStore(app) // 初始化状态管理
