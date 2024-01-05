37bd1a38432408aec3f43c086017481cac78ed1a
TomDiary <andihankai@gmail.com>
2024-01-05 10:27:40 +0800

    fix: Adjust import method

--- mock/_createServer.js
+++ mock/_createServer.js
@@ -1,14 +1,19 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
+import mapMock from './modules/map'
+import menuMock from './modules/menu'
+import roleMock from './modules/role'
+import tableMock from './modules/table'
+import userMock from './modules/user'
+import vehicleMock from './modules/vehicle'
 
-const mockList = []
-const modules = import.meta.globEager('./**/*.js')
-
-Object.keys(modules).forEach((key) => {
-  if (key.includes('/_')) {
-    return
-  }
-  mockList.push(...modules[key].default)
-})
+const mockList = [
+  ...mapMock,
+  ...menuMock,
+  ...roleMock,
+  ...tableMock,
+  ...userMock,
+  ...vehicleMock
+]
 
 export function setupProdMockServer() {
   createProdMockServer(mockList)
